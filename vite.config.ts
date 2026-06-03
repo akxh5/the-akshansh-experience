f8c2155262a063d338fe57017892bb8abf075abc
Akshansh Sharma <aksh11ansh@gmail.com>
2026-06-03 20:16:02 +0530

    build: explicitly add nitro plugin for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,13 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 import mdx from "@mdx-js/rollup";
+import { nitro } from "nitro/vite"; // <-- 1. Import the Nitro plugin
 
 export default defineConfig({
   plugins: [
-    tanstackStart({
-      server: { preset: "vercel" },
+    tanstackStart(),
+    nitro({
+      preset: "vercel" // <-- 2. Tell Nitro to optimize specifically for Vercel
     }),
     react(),
     tailwindcss(),
